6aaab019089928f34231a4b2d2c04ba4e4d6f38b
MahmoudFredj <mahmoudrayfredj@gmail.com>
2021-03-23 19:09:49 +0100

    intial commit

---
+++ Demo/Calc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace Demo
+{
+    class Calc
+    {
+        public static object tailRecursionFactorial(int n,BigInteger acc)
+        {
+            if (n == 0) return TailRecursion.Return(acc);
+            return TailRecursion.Next(() =>(RecursionResult < BigInteger >) tailRecursionFactorial(n - 1, n * acc));
+        }
+
+        public static BigInteger Factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n*Factorial(n - 1);
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factValue = 50000;
+
+            object result = TailRecursion.Execute(() => (RecursionResult<BigInteger>)Calc.tailRecursionFactorial(factValue, 1));
+            Console.WriteLine(result);
+
+            BigInteger overflowRes = Calc.Factorial(factValue);
+            Console.WriteLine(overflowRes);
+        }
+    }
+}
